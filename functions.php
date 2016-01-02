2f9816f6780365c64f144664e0cc8375e532d9a7
Leland Fiegel <leland@Lelands-MacBook.fios-router.home>
2016-01-02 10:26:34 -0500

    Remove Jetpack logo functions

--- functions.php
+++ functions.php
@@ -9,6 +9,28 @@
  * Enqueue styles.
  */
 function adaline_logo_letter_enqueue_styles() {
+
+	// Load the parent theme stylesheet
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
+
+} // end function adaline_logo_letter_enqueue_styles
 add_action( 'wp_enqueue_scripts', 'adaline_logo_letter_enqueue_styles' );
+
+/**
+ * Remove unneeded Jetpack Logo related functions.
+ * Hook into after_theme_setup with priority of 11
+ * to override parent theme functions.
+ *
+ * @link https://codex.wordpress.org/Function_Reference/remove_theme_support
+ *
+ */
+function adaline_logo_letter_setup() {
+
+	// This will remove the image size used exclusively with Jetpack Site Logo
+	remove_image_size( 'adaline-logo' );
+
+	// This will remove theme support for Jetpack Site Logo
+	remove_theme_support( 'site-logo' );
+
+} // end function adaline_logo_letter_setup
+add_action( 'after_setup_theme', 'adaline_logo_letter_setup', 11 );
